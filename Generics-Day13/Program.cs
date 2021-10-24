3ace1520b2e1127efa1e7350160d509d490abe56
Prajwal <prajwal.zurunge12@gmail.com>
2021-10-24 17:25:47 +0530

    adding finding maximum number program

--- Generics-Day13/Program.cs
+++ Generics-Day13/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace Generics_Day13
+namespace FindMax
 {
-    class Program
+    class MaxNumbers
     {
-        static void Main(string[] args)
+        int[] intNums = { 10, 30, 20 };
+        public void MaxInt()
         {
-            Console.WriteLine("Welcome To Generics problems");
+            for (int i = 0; i < intNums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < intNums.Length; j++)
+                {
+                    if (intNums[i] < intNums[j])
+                    {
+                        int temp = intNums[i];
+                        intNums[i] = intNums[j];
+                        intNums[j] = temp;
+                    }
+                }
+            }
+        }
+        public void Show()
+        {
+            for (int i = 0; i < intNums.Length; i++)
+            {
+                Console.WriteLine(intNums[i]);
+            }
         }
     }
 }
