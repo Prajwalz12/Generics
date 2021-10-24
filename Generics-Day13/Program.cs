87251a6dcb8ab224465d1105b91b568626ec45ba
Prajwal <prajwal.zurunge12@gmail.com>
2021-10-24 17:17:15 +0530

    Welcome to Generics Problems

---
+++ Generics-Day13/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generics_Day13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Generics problems");
+        }
+    }
+}
